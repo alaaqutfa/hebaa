dd11f30add6cd9100d62ec7f717bab7582a51527
alaaqutfa <alaaqutfa.work@gmail.com>
2025-06-12 15:26:32 +0300

    edit web design

--- resources/views/web/layouts/partials/header.blade.php
+++ resources/views/web/layouts/partials/header.blade.php
@@ -6,7 +6,7 @@
                   <img src="{{ asset('assets/img/logo.png') }}" alt="شعار حباء" />
               </a>
 
-              <div class="mobile-side flex gap-2">
+              <div class="mobile-side">
                   <!-- Mobile hamburger -->
                   <button type="button" class="hamburger" aria-label="Menu" onclick="mobileNavToogle()">
                       <svg class="list-icon w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
@@ -32,6 +32,19 @@
                               <transition name="fade">
                                   <ul v-if="isProfileMenuOpen" @click.self="closeProfileMenu"
                                       @keydown.esc="closeProfileMenu" class="header-profile-list" aria-label="submenu">
+                                      @if (Auth::user()->is_admin)
+                                          <li>
+                                              <a href="{{ route('admin.dashboard') }}">
+                                                  <svg class="w-4 h-4 mr-3" aria-hidden="true" fill="none"
+                                                      stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                      viewBox="0 0 24 24" stroke="currentColor">
+                                                      <path d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"></path>
+                                                      <path d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"></path>
+                                                  </svg>
+                                                  <span>{{ translation('Dashboard') }}</span>
+                                              </a>
+                                          </li>
+                                      @endif
                                       <li>
                                           <a href="#">
                                               <svg class="w-4 h-4 mr-3" fill="none" stroke="currentColor"
@@ -41,18 +54,6 @@
                                                       d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                                               </svg>
                                               <span>{{ translation('Profile') }}</span>
-                                          </a>
-                                      </li>
-                                      <li>
-                                          <a href="#">
-                                              <svg class="w-4 h-4 mr-3" fill="none" stroke="currentColor"
-                                                  stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round"
-                                                  stroke-linejoin="round">
-                                                  <path
-                                                      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                                                  <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                              </svg>
-                                              <span>{{ translation('Settings') }}</span>
                                           </a>
                                       </li>
                                       <li>
